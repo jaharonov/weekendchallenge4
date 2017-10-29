c2cc067f891b7195c07f0dbe57db8755bdff0cbf
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-29 17:37:36 -0500

    real photos and captions added to objects

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,17 +5,25 @@ myApp.controller('picController', function () {
     
     pic.showPictures = true;
     pic.message = 'show pics!';
-
     pic.display = function (picture) {
-        // person.show = !person.show;
-        console.log(pic.name);
-        console.log(pic.file);
-        picture = pictures.push({ name: pic.name, file: pic.file});
+        
+        picture = pictures.push({ name: pic.name, file: pic.file, caption: pic.caption});
+        
     }
 
 
-    var pictures = [{ name: 'image1', file: '/images/pug1.jpg'},
-    {name: 'image2', file: '/images/pug2.gif'}];
+    var pictures = [{ name: 'image1', file: '/images/biggie_crystals.jpg', caption: 'My cat, Biggie!'},
+    {name: 'image2', file: '/images/glacier_sunrise.jpg', caption: 'Sunrise while camping at Glacier National Park.'},
+    {name: 'image3', file: '/images/kelley_canoe.jpg', caption: 'Canoeing in Minneapolis.'},
+    {name: 'image4', file: '/images/lacroix.jpg', caption: 'Just drinking a La Croix in the summer!'},
+    {name: 'image5', file: '/images/pottery.jpg', caption: 'My first attempts at pottery.'},
+    {name: 'image6', file: '/images/with_kelley.jpg', caption: 'Being a dork with Kelley.' }];
+
+    pic.showCaption = function () {
+    
+     picture.caption = !picture.caption;
+
+    }
     
     
 
