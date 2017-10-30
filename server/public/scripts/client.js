b1869272e9bdb0ccb6b0d3aaccc5250287de0cf5
Jaffa Aharonov <jaharonov@gmail.com>
2017-10-30 07:06:39 -0500

    basemode complete except for caption repeating issue

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,21 +3,21 @@ var myApp = angular.module('myApp', []);
 myApp.controller('picController', function () {
     var pic = this;
     
-    pic.showCaption = false;
+    
     pic.message = 'show pics!';
     pic.display = function (picture) {
         
-    var picture = pictures.push({ name: pic.name, file: pic.file, caption: pic.caption});
+    var picture = pictures.push({ name: pic.name, file: pic.file, caption: pic.caption, show: pic.show});
         
     }
 
 
-    var pictures = [{ name: 'image1', file: '/images/biggie_crystals.jpg', caption: "My cat, Biggie!"},
-    {name: 'image2', file: '/images/glacier_sunrise.jpg', caption: "Sunrise while camping at Glacier National Park."},
-    {name: 'image3', file: '/images/kelley_canoe.jpg', caption: "Canoeing in Minneapolis."},
-    {name: 'image4', file: '/images/lacroix.jpg', caption: "Just drinking a La Croix in the summer!"},
-    {name: 'image5', file: '/images/pottery.jpg', caption: "My first attempts at pottery."},
-    {name: 'image6', file: '/images/with_kelley.jpg', caption: "Being a dork with Kelley." }];
+    var pictures = [{ name: 'image1', file: '/images/biggie_crystals.jpg', caption: "My cat, Biggie!", show: false},
+    {name: 'image2', file: '/images/glacier_sunrise.jpg', caption: "Sunrise while camping at Glacier National Park.", show: false},
+    { name: 'image3', file: '/images/kelley_canoe.jpg', caption: "Canoeing in Minneapolis.", show: false},
+    { name: 'image4', file: '/images/lacroix.jpg', caption: "Just drinking a La Croix in the summer!", show: false},
+    { name: 'image5', file: '/images/pottery.jpg', caption: "My first attempts at pottery.", show: false},
+    { name: 'image6', file: '/images/with_kelley.jpg', caption: "Being a dork with Kelley.",show: false }];
 
     pic.showCaption = function (picture) {
        
